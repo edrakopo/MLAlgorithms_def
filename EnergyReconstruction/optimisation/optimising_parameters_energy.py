a16e3806bfaaff9e082c9e43fd9366408e0cec97
edrakopo <edrakopo@staffmail.ed.ac.uk>
2019-04-11 16:05:00 +0100

    Regression projects - Energy & Track Length Reconstruction

--- EnergyReconstruction/optimisation/optimising_parameters_energy.py
+++ EnergyReconstruction/optimisation/optimising_parameters_energy.py
@@ -1,9 +1,11 @@
+#optimising parameters for Energy BDT
+
 ##### Script for Muon Energy Reconstruction in the water tank
 #import Store
 import sys
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+#import tensorflow as tf
 import tempfile
 import random
 import csv
@@ -19,6 +21,9 @@ import sklearn
 from sklearn.utils import shuffle
 from sklearn import linear_model, ensemble
 from sklearn.metrics import mean_squared_error
+import pickle
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 #import seaborn as sns
 
 #import ROOT
@@ -28,7 +33,7 @@ from sklearn.metrics import mean_squared_error
 
 #-------- File with events for reconstruction:
 #--- evts for training:
-infile = "../../LocalFolder/vars_Ereco.csv"
+infile = "../../../LocalFolder/vars_Ereco.csv"
 #----------------
 
 # Set TF random seed to improve reproducibility
@@ -71,60 +76,64 @@ arr_hi_E0B = arr_hi_E0[rnd_indices]
 arr2_hi_E_n = arr_hi_E0B #.reshape(arr_hi_E0B.shape + (-1,))
 arr3_hi_E = arr3_hi_E0[rnd_indices]
 #--- select events for prediction: -- in future we need to replace this with data sample!
-evts_to_predict = arr_hi_E0[~rnd_indices]
-evts_to_predict_n = evts_to_predict #.reshape(evts_to_predict.shape + (-1,))
-test_data_trueKE_hi_E = arr3_hi_E0[~rnd_indices]
+#evts_to_predict = arr_hi_E0[~rnd_indices]
+#evts_to_predict_n = evts_to_predict #.reshape(evts_to_predict.shape + (-1,))
+#test_data_trueKE_hi_E = arr3_hi_E0[~rnd_indices]
 
 #printing..
-print('events for training: ',len(arr3_hi_E),' events for predicting: ',len(test_data_trueKE_hi_E)) 
-print('initial train shape: ',arr3_hi_E.shape," predict: ",test_data_trueKE_hi_E.shape)
+#print('events for training: ',len(arr3_hi_E),' events for predicting: ',len(test_data_trueKE_hi_E))
+print('initial train shape: ',arr3_hi_E.shape," arr2_hi_E_n.shape: ",arr2_hi_E_n.shape)
 
-########### BDTG ############
-n_estimators=1000
-params = {'n_estimators':n_estimators, 'max_depth': 50,
-          'learning_rate': 0.01, 'loss': 'lad'} 
-    
-print("arr2_hi_E_n.shape: ",arr2_hi_E_n.shape)
 #--- select 70% of sample for training and 30% for testing:
 offset = int(arr2_hi_E_n.shape[0] * 0.7) 
 arr2_hi_E_train, arr3_hi_E_train = arr2_hi_E_n[:offset], arr3_hi_E[:offset].reshape(-1)  # train sample
 arr2_hi_E_test, arr3_hi_E_test   = arr2_hi_E_n[offset:], arr3_hi_E[offset:].reshape(-1)  # test sample
- 
+  
 print("train shape: ", arr2_hi_E_train.shape," label: ",arr3_hi_E_train.shape)
 print("test shape: ", arr2_hi_E_test.shape," label: ",arr3_hi_E_test.shape)
+ 
+# ------ optimise model ------ 
+def optimise_model(model, alg_name, search_method, params):
+
+    #---- GridSearchCV ----
+    random_search = GridSearchCV(model, param_grid=params, scoring='neg_mean_squared_error', n_jobs=4, cv=5, verbose=3)
+    random_search.fit(arr2_hi_E_train, arr3_hi_E_train)
+
+    print("------ Algorithm: " + str(alg_name))
+    #print('\n All results:')
+    #print(random_search.cv_results_)
+    print('\n Best estimator:')
+    print(random_search.best_estimator_)
+    print('\n Best hyperparameters:')
+    print(random_search.best_params_)
+    #results = pd.DataFrame(random_search.cv_results_)
+    #results.to_csv('xgb-random-grid-search-results-01.csv', index=False)
+    print("----------------------------------------------")
+
+
+########### BDTG ############
+params = {'n_estimators':[100, 200, 500, 600, 1000], 
+          'max_depth': [10, 50, 100],
+          'learning_rate': [0.01, 0.025, 0.05, 0.1], 
+          'loss': ['lad']} 
     
-print("training BDTG...")
 net_hi_E = ensemble.GradientBoostingRegressor(**params)
-model = net_hi_E.fit(arr2_hi_E_train, arr3_hi_E_train)
-net_hi_E
+#net_hi_E.fit(arr2_hi_E_train, arr3_hi_E_train)
+optimise_model(net_hi_E, " BDTG ", "grid", params)
+#---------------------------#
 
-# save the model to disk
-filename = 'finalized_BDTmodel_forMuonEnergy.sav'
-pickle.dump(model, open(filename, 'wb'))
+## save the model to disk
+#filename = 'finalized_BDTmodel_forMuonEnergy.sav'
+#pickle.dump(model, open(filename, 'wb'))
 
-mse = mean_squared_error(arr3_hi_E_test, net_hi_E.predict(arr2_hi_E_test)) 
-print("MSE: %.4f" % mse)
-print("events at training & test samples: ", len(arr_hi_E0))
-print("events at train sample: ", len(arr2_hi_E_train))
-print("events at test sample: ", len(arr2_hi_E_test))
+#mse = mean_squared_error(arr3_hi_E_test, net_hi_E.predict(arr2_hi_E_test)) 
+#print("MSE: %.4f" % mse)
+#print("events at training & test samples: ", len(arr_hi_E0))
+#print("events at train sample: ", len(arr2_hi_E_train))
+#print("events at test sample: ", len(arr2_hi_E_test))
  
-test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
+#test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
  
-for i, y_pred in enumerate(net_hi_E.staged_predict(arr2_hi_E_test)):
-    test_score[i] = net_hi_E.loss_(arr3_hi_E_test, y_pred)
-
-#    fig,ax=plt.subplots(ncols=1, sharey=True)
-#    ax.plot(np.arange(params['n_estimators']) + 1, net_hi_E.train_score_, 'b-',
-#             label='Training Set Deviance')
-#    ax.plot(np.arange(params['n_estimators']) + 1, test_score, 'r-',
-#             label='Test Set Deviance')
-#     ax.set_ylim(0.,500.)
-#     ax.set_xlim(0.,n_estimators)
-#     ax.legend(loc='upper right')
-#     ax.set_ylabel('Least Absolute Deviations [MeV]')
-#     ax.set_xlabel('Number of Estimators')
-#     ax.yaxis.set_label_coords(-0.1, 0.6)
-#     ax.xaxis.set_label_coords(0.85, -0.08)
-#     plt.savefig("deviation_train_test.png")
-
+#for i, y_pred in enumerate(net_hi_E.staged_predict(arr2_hi_E_test)):
+#    test_score[i] = net_hi_E.loss_(arr3_hi_E_test, y_pred)
 
