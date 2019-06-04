534fa9230911124eb271d7565455bbbc9e141fbe
edrakopo <edrakopo@staffmail.ed.ac.uk>
2019-06-04 12:54:52 +0100

    Regression projects - Energy & Track Length Reconstruction

--- EnergyReconstruction/separate_training_prediction/BDT_MuonEnergyReco_pred.py
+++ EnergyReconstruction/separate_training_prediction/BDT_MuonEnergyReco_pred.py
@@ -47,19 +47,6 @@ bins = int((E_high-E_low)/div)
 print('bins: ', bins)
 
 print( "--- opening file with input variables!") 
-#--- events for training ---
-#filein = open(str(infile))
-##print("evts for training in: ",filein)
-#df00=pd.read_csv(filein)
-#df0=df00[['totalPMTs','totalLAPPDs','TrueTrackLengthInWater','neutrinoE','trueKE','diffDirAbs','TrueTrackLengthInMrd','recoDWallR','recoDWallZ','dirX','dirY','dirZ','vtxX','vtxY','vtxZ','DNNRecoLength']]
-##dfsel=df0.loc[df0['neutrinoE'] < E_threshold]
-#dfsel = df0
-
-#print to check:
-#print("check training sample: ",dfsel.head())
-#print(dfsel.iloc[5:10,0:5])
-#check fr NaN values:
-#assert(dfsel.isnull().any().any()==False)
 
 #--- events for predicting ---
 filein2 = open(str(infile2))
@@ -74,32 +61,9 @@ print("check predicting sample: ",dfsel_pred.shape," ",dfsel_pred.head())
 #check fr NaN values:
 assert(dfsel_pred.isnull().any().any()==False)
 
-#--- normalisation-training sample:
-##dfsel_n = pd.DataFrame([ dfsel['DNNRecoLength']/600., dfsel['TrueTrackLengthInMrd']/200., dfsel['diffDirAbs'], dfsel['recoDWallR']/152.4, dfsel['recoDWallZ']/198., dfsel['totalLAPPDs']/1000., dfsel['totalPMTs']/1000., dfsel['vtxX']/150., dfsel['vtxY']/200., dfsel['vtxZ']/150. ]).T
-#dfsel_n = pd.DataFrame([ dfsel['DNNRecoLength']/600., dfsel['TrueTrackLengthInMrd']/200., dfsel['diffDirAbs'], dfsel['recoDWallR'], dfsel['recoDWallZ'], dfsel['totalLAPPDs']/200., dfsel['totalPMTs']/200., dfsel['vtxX']/150., dfsel['vtxY']/200., dfsel['vtxZ']/150. ]).T
-#print("chehck normalisation: ", dfsel_n.head())
 #--- normalisation-sample for prediction:
 #dfsel_pred_n = pd.DataFrame([ dfsel_pred['DNNRecoLength']/600., dfsel_pred['TrueTrackLengthInMrd']/200., dfsel_pred['diffDirAbs'], dfsel_pred['recoDWallR']/152.4, dfsel_pred['recoDWallZ']/198., dfsel_pred['totalLAPPDs']/1000., dfsel_pred['totalPMTs']/1000., dfsel_pred['vtxX']/150., dfsel_pred['vtxY']/200., dfsel_pred['vtxZ']/150. ]).T
 dfsel_pred_n = pd.DataFrame([ dfsel_pred['DNNRecoLength']/600., dfsel_pred['TrueTrackLengthInMrd']/200., dfsel_pred['diffDirAbs'], dfsel_pred['recoDWallR'], dfsel_pred['recoDWallZ'], dfsel_pred['totalLAPPDs']/200., dfsel_pred['totalPMTs']/200., dfsel_pred['vtxX']/150., dfsel_pred['vtxY']/200., dfsel_pred['vtxZ']/150. ]).T
-
-#--- prepare training & test sample for BDT:
-#arr_hi_E0 = np.array(dfsel_n[['DNNRecoLength','TrueTrackLengthInMrd','diffDirAbs','recoDWallR','recoDWallZ','totalLAPPDs','totalPMTs','vtxX','vtxY','vtxZ']])
-#arr3_hi_E0 = np.array(dfsel[['trueKE']])
- 
-##---- random split of events ----
-#rnd_indices = np.random.rand(len(arr_hi_E0)) < 0.50
-##--- select events for training/test:
-#arr_hi_E0B = arr_hi_E0[rnd_indices]
-#arr2_hi_E_n = arr_hi_E0B #.reshape(arr_hi_E0B.shape + (-1,))
-#arr3_hi_E = arr3_hi_E0[rnd_indices]
-##--- select events for prediction: -- in future we need to replace this with data sample!
-#evts_to_predict = arr_hi_E0[~rnd_indices]
-#evts_to_predict_n = evts_to_predict #.reshape(evts_to_predict.shape + (-1,))
-#test_data_trueKE_hi_E = arr3_hi_E0[~rnd_indices]
-
-#printing..
-#print('events for training: ',len(arr3_hi_E),' events for predicting: ',len(test_data_trueKE_hi_E)) 
-#print('initial train shape: ',arr3_hi_E.shape," predict: ",test_data_trueKE_hi_E.shape)
 
 #prepare events for predicting 
 evts_to_predict_n= np.array(dfsel_pred_n[['DNNRecoLength','TrueTrackLengthInMrd','diffDirAbs','recoDWallR','recoDWallZ','totalLAPPDs','totalPMTs', 'vtxX','vtxY','vtxZ']])
@@ -107,18 +71,14 @@ test_data_trueKE_hi_E = np.array(dfsel_pred[['trueKE']])
 
 n_estimators=1000
 
-# save the model to disk
-filename = 'finalized_BDTmodel_forMuonEnergy.sav'
-#pickle.dump(model, open(filename, 'wb'))
-
 # load the model from disk
+filename = 'finalized_BDTmodel_forMuonEnergy.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 #result = loaded_model.score(X_test, Y_test)
 #print(result)
 
 #predicting...
 print("events for energy reco: ", len(evts_to_predict_n)) 
-#BDTGoutput_E = net_hi_E.predict(evts_to_predict_n)
 BDTGoutput_E = loaded_model.predict(evts_to_predict_n)
 
 Y=[0 for j in range (0,len(test_data_trueKE_hi_E))]
@@ -151,14 +111,4 @@ title = "mean = %.2f, std = %.2f " % (np.array(Y).mean(), np.array(Y).std())
 plt.title(title)
 plt.savefig("DE_E.png")
  
-#write in output .root file:
-#vtxX=dfsel_n['vtxX'][~rnd_indices]
-#vtxY=dfsel_n['vtxY'][~rnd_indices]
-#vtxZ=dfsel_n['vtxZ'][~rnd_indices]
-#outputFile = ROOT.TFile.Open("TESTNEWOUTRecoMuonMRD.root", "RECREATE")
-#outputTuple = ROOT.TNtuple("tuple", "tuple", "trueKE:recoKE:vtxX:vtxY:vtxZ:DE_E")
-#for i in range(len(test_data_trueKE_hi_E)):
-#    outputTuple.Fill(test_data_trueKE_hi_E[i],BDTGoutput_E[i],vtxX[i],vtxY[i],vtxZ[i],Y[i])
-#outputTuple.Write()
-#outputFile.Close()
 
