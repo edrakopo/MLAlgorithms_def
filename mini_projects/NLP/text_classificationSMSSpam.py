231b306029bd9d6d218bcd2385e758821f26d764
edrakopo <edrakopo@staffmail.ed.ac.uk>
2020-04-05 00:55:31 +0100

    first NLP mini projects

--- mini_projects/NLP/text_classificationSMSSpam.py
+++ mini_projects/NLP/text_classificationSMSSpam.py
@@ -1,125 +1,80 @@
 import nltk
-#nltk.download() 
-#nltk.download('stopwords')
-#nltk.download('wordnet')
-
 import pandas as pd
-#Reading file:
-data = pd.read_csv('SMSSpamCollection.csv', sep='\t', names=['text'], skiprows=1,header=None)
-data = pd.DataFrame(data.text.str.split(',',1).tolist(),
-                                   columns = ['labels','text'])
-print(data.head())
-#print(data.labels)
-
-### ---- Preprocessing data --- ###
-#Remove Punctuation
-import string
-print("punctuation: ",string.punctuation)
-
-#function to remove punctuation:
-def remove_punct(text):
-    text_nopunct = "".join([char for char in text if char not in string.punctuation])
-    #text_nopunct = text.replace((char,"") for char in text if char not in string.punctuation)
-    return text_nopunct
-
-data['body_text_clean'] = data.text.apply(lambda x:remove_punct(x))
-print(data.head())
-
-#Tokenization
 import re
-
-#function to tokenize words:
-def tokenize(text):
-    tokens = re.split('\W+',text) #\W+ means that either a word character(A-Za-z0-9_) or a dash(-) can go there
-    return tokens
-
-data['text_tokenized'] = data.text.apply(lambda x:tokenize(x))
-print(data.head())
-
-#Remove stopwords
-stopword = nltk.corpus.stopwords.words('english') #all english stopwords
-
-def remove_stopwords(tokenized_list):
-    text = [word for word in tokenized_list if word not in stopword]
-    return text
-
-data['text_nostopwords'] = data['text_tokenized'].apply(lambda x:remove_stopwords(x))
-print(data.head())
-
-#Stemming -reduce a word to its stem form
-ps = nltk.PorterStemmer()
-
-def stemming(tokenized_text):
-    text = [ps.stem(word) for word in tokenized_text]
-    return text
-
-data['text_stemmed'] = data['text_nostopwords'].apply(lambda x:stemming(x))
-print(data.head())
-
-#Lemmatizing
-#lemmatizing is more accurate than stemming as it uses a dictionary-based approach e.g. Entitling, Entitled->Entitle instead of Entitl
-wn = nltk.WordNetLemmatizer()
-
-def lemmatizing(tokenized_text):
-    text = [wn.lemmatize(word) for word in tokenized_text]
-    return text
-
-data['text_lemmatized'] = data['text_nostopwords'].apply(lambda x:lemmatizing(x))
-print(data.head())
-
-### -------------------- ###
-
-### ---- Vectorizing Data: Bag-Of-Words ---- ###
-import sklearn
-from sklearn.feature_extraction.text import CountVectorizer
-import sklearn.feature_extraction.text
-
-count_vect = CountVectorizer()
-X_counts = count_vect.fit_transform(data['text'])	 
-#X_counts = count_vect.fit_transform(data['text'])
-print(X_counts.shape)
-#print(count_vect.get_feature_names())
-
-### ---- Vectorizing Data: N-Grams ---- ###
-from sklearn.feature_extraction.text import CountVectorizer
-
-#ngram_vect = CountVectorizer(ngram_range(2,2),analyzer=clean_text) #It applies only bigram vectorizer
-#X_counts = ngram_vect.fit_transform(data['text']) 
-#print(X_counts.shape)
-
-### ---- Vectorizing Data: TF-IDF ---- ###
-#from sklearn.feature_extraction.text import TfidfVectorizer
-
-#tfidf_vect = TfidfVectorizer(analyzer=clean_text)
-#X_tfidf = tfidf_vect.fit_transform(data['text'])
-#print(X_tfidf.shape)
-#print(tfidf_vect.get_feature_names())
-
-### ---- Feature Engineering: Feature Creation ---- ###
-#create feature for text message length and % of punctuation in text
+from sklearn.feature_extraction.text import TfidfVectorizer
 import string
 
-#Function to calculate length of message excluding space
-data['body_length'] = data['text'].apply(lambda x: len(x)-x.count(" "))
+data = pd.read_csv("SMSSpamCollection.csv", names=['body_text'], sep='\t',skiprows=1,header=None)
+data = pd.DataFrame(data.body_text.str.split(',',1).tolist(),
+                                   columns = ['label','body_text'])
 print(data.head())
+print(data['label'].value_counts())
+
+#Preprocessing data
+stopwords = nltk.corpus.stopwords.words('english')
+ps = nltk.PorterStemmer() #reduces the words to their root
 
 def count_punct(text):
     count = sum([1 for char in text if char in string.punctuation])
-    return round(count/(len(text) - text.count(" ")), 3)*100.
+    return round(count/(len(text) - text.count(" ")), 3)*100
 
-data['punct%'] = data['text'].apply(lambda x:count_punct(x))
-print(data.head())
+data['body_len'] = data['body_text'].apply(lambda x: len(x) - x.count(" "))
+data['punct%'] = data['body_text'].apply(lambda x: count_punct(x))
 
-#check if features are good or not:
-import numpy as np
+def clean_text(text):
+    text = "".join([word.lower() for word in text if word not in string.punctuation])
+    tokens = re.split('\W+', text)
+    text = [ps.stem(word) for word in tokens if word not in stopwords]
+    return text
+
+#Split into train/test
+from sklearn.model_selection import train_test_split
+
+X=data[['body_text', 'body_len', 'punct%']]
+y=data['label']
+print("Text Ready: ", data.head())
+print("check: ", X.shape, "," , y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+#Vectorise text
+tfidf_vect = TfidfVectorizer(analyzer=clean_text)
+tfidf_vect_fit = tfidf_vect.fit(X_train['body_text'])
+
+tfidf_train = tfidf_vect_fit.transform(X_train['body_text'])
+tfidf_test = tfidf_vect_fit.transform(X_test['body_text'])
+
+X_train_vect = pd.concat([X_train[['body_len', 'punct%']].reset_index(drop=True), 
+           pd.DataFrame(tfidf_train.toarray())], axis=1)
+X_test_vect = pd.concat([X_test[['body_len', 'punct%']].reset_index(drop=True), 
+           pd.DataFrame(tfidf_test.toarray())], axis=1)
+
+print(X_train_vect.head())
+
+#Evaluation of Models
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.metrics import accuracy_score as acs
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-bins = np.linspace(0,200,40)
-#print(data[data['labels']=='0']['body_length'].head())
+rf = RandomForestClassifier(n_estimators=150, max_depth=None, n_jobs=-1)
 
-plt.hist(data[data['labels']=='1']['body_length'], bins, alpha=0.5, density=True, label='spam')
-plt.hist(data[data['labels']=='0']['body_length'], bins, alpha=0.5, density=True, label='ham')
-plt.legend(loc='upper left')
+rf_model = rf.fit(X_train_vect, y_train)
+
+y_pred = rf_model.predict(X_test_vect)
+
+precision, recall, fscore, train_support = score(y_test, y_pred, pos_label='1', average='binary')
+print('Precision: {} / Recall: {} / F1-Score: {} / Accuracy: {}'.format(
+    round(precision, 3), round(recall, 3), round(fscore,3), round(acs(y_test,y_pred), 3)))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+class_label = ["ham", "spam"]
+df_cm = pd.DataFrame(cm, index=class_label,columns=class_label)
+sns.heatmap(df_cm, annot=True, fmt='d')
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
 plt.show()
-#comment: Spams have a high number of words as compared to Hams. So it’s a good feature to distinguish
-
